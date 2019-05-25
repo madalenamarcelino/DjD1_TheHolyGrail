a04a45f42876c7d813b45f429ce6082d1723640a
Unknown <pedrooliveira2007@hotmail.com>
2019-05-25 16:55:36 +0100

    added everything
    o antigo repositorio foi apagado por algum motivo desconhecido, foi criado um novo e foi adicionado todos os arquivos novamente, antiga pasta .git está em aqruivo zip para consulta

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Vector3 velocity;
+
+    public float _smoothTimeX = 0;
+    public float _smoothTimeY = 0;
+
+    public GameObject player;
+    public bool _bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    void FixedUpdate()
+    {
+        float _posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, _smoothTimeX);
+        float _posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, _smoothTimeY);
+
+        transform.position = new Vector3(_posX, _posY, transform.position.z);
+
+        if (_bounds)
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+
+    }
+
+}
---
+++ Assets/scripts/CameraPos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    //Positions
+    public Transform currentTarget;
+    public Transform player;
+    public Transform pos1;
+    public Transform pos2;
+
+    [Range (0f, 1.0f)]
+    public float smoothSpeed = 0.1f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        currentTarget = player;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            currentTarget = pos1;
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            currentTarget = pos2;
+
+
+        //Follow target
+        Vector3 desiredPos = currentTarget.position + offset;
+        Vector3 smoothedPos = Vector3.LerpUnclamped(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPos;
+
+        transform.LookAt(currentTarget);
+    }
+}
